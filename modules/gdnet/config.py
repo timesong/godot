f4b577c5e388880c94c17bc71d67c74efea2bbea
Zhang Kun <timesong@gmail.com>
2017-06-01 09:02:51 +0800

    add iOS & Android support for Gdnet

--- modules/gdnet/config.py
+++ modules/gdnet/config.py
@@ -1,5 +1,5 @@
 def can_build(platform):
-	return (platform == "x11" or platform == "server" or platform == "windows" or platform == "osx")  
+	return (platform == "x11" or platform == "server" or platform == "windows" or platform == "osx" or platform == "iphone" or platform == "android")  
 
 def configure(env):
 	pass
