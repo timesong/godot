b00e511f9a1a16f572fac13786c0f2475773b9b5
Zhang Kun <timesong@gmail.com>
2017-08-12 13:46:12 +0800

    new upstream source

--- modules/gdnet/config.py
+++ modules/gdnet/config.py
@@ -1,5 +1,5 @@
 def can_build(platform):
-	return (platform == "x11" or platform == "server" or platform == "windows" or platform == "osx" or platform == "iphone" or platform == "android")  
+	return True
 
 def configure(env):
 	pass
